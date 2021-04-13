20506d69868a6f4cee9d9b0930a3bcd8ca69c738
Anadbh <anafruiz20@gmail.com>
2021-04-13 10:43:10 +0200

    Update server

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 const isDevEnviroment = process.env.NODE_ENV === "development"; //Booleano
 const apiUrl = isDevEnviroment
   ? "http://localhost:3000"
-  : "https://awesome-profile-cards-magician.herokuapp.com";
+  : "https://anafruiz-react-magicians.herokuapp.com/";
 
 const sendToApi = (data) => {
   return fetch(`${apiUrl}/card`, {
